9b525f684914b12b5ead7e02c627ab0998e63abe
ReygenChristianM <terserahpangoni@gmail.com>
2020-08-25 14:31:34 +0800

    pertemuan 3

---
+++ Pertemuan_3/index.js
@@ -0,0 +1,38 @@
+/*
+DASAR-DASAR PEMOGRAMAN JAVASCRIPT
+1. Variabel dan tipe data
+2. Operator
+3. Array
+4. Object
+5. Fungsi
+6. JavaScript Conditional
+7. JavaScript Loop
+*/
+
+//=======================================================================================================
+
+/*
+1. Variabel dan tipe data
+ */
+const namaDepan ="Reygen";//tipe data string
+let umur= 19;//tipe data number
+umur=20;
+const SudahMakan= false;//tipe data boolean
+let jumlahAnak;//undefined
+console.log(namaDepan);
+console.log(SudahMakan);
+console.log(umur);
+console.log("My name is "+namaDepan +", I'm "+umur+" years old "+SudahMakan);
+
+const info = "My name is "+namaDepan +", I'm "+umur+" years old "+SudahMakan
+console.log("data type of info is : "+typeof info)//type coersion
+
+/*
+2. Operator
+*/
+
+let bilangan= 10;
+
+console.log(bilangan =="10");//loose equality
+console.log(bilangan ==="10");//strict equality
+
