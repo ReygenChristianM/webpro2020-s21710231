b5285ae4b20eb7721aa66de2c6b7add11e6943d4
ReygenChristianM <terserahpangoni@gmail.com>
2020-08-27 14:11:48 +0800

    pertemuan 4

---
+++ Pertemuan_4/index.js
@@ -0,0 +1,25 @@
+/*
+    1.function
+ */
+//function declaration
+function penjumlahanDuaBujurSangkar(a, b){
+    return a*a+b*b;
+}
+
+//function expression
+// penjumlahanDuaBujurSangkar=function(a, b){
+//     return a*a+b*b;
+// }
+
+console.log(penjumlahanDuaBujurSangkar(8, 3))
+
+
+function tambah(a,b){
+    return a+b;
+}
+
+function kali(a,b)
+{
+    return a*b;
+}
+console.log(tambah(5*2,kali(10,2)))
