b4ff5fb41e5bf45ad677e1131d04380ce43e1e84
ReygenChristianM <terserahpangoni@gmail.com>
2020-09-24 13:02:29 +0800

    Pertemuan ke-11

---
+++ Pertemuan_11/index.js
@@ -0,0 +1,27 @@
+/*
+    Arrow Function
+ */
+
+ //Deklarasi fungsi biasa
+function ucapkanSalam() {
+    return "Selamat Pagi";
+  };
+  
+  //Dengan Arrow Function
+
+  // fungsi dengan parameter
+const operasiPenjumlahan = (bilangan1, bilangan2) => {
+    const hasil =  bilangan1 + bilangan2;
+    return hasil;
+  };
+  
+  console.log(operasiPenjumlahan(3, 4)); // Output: 7
+  
+  // fungsi yang tidak memiliki parameter
+  const namaJenisAnjing = () => {
+    const anjing = ["Pug", "Bulldog", "Poodle"];
+    return anjing[Math.floor(Math.random()*(anjing.length))];
+  }
+  
+  console.log(namaJenisAnjing()); // Output: Pug (hasil random)
+  
