b9280b64c9912a8bffe8ffc07020f774ae431b94
ReygenChristianM <terserahpangoni@gmail.com>
2020-11-11 13:51:16 +0800

    Pertemuan_21&22_merge

---
+++ Pertemuan21_22/p/src/utils/person.js
@@ -0,0 +1,26 @@
+export const person =[
+    {
+        name:"Reygen Moningka",
+        institution:"Universitas Klabat",
+        address:"Lembean - Minahasa Utara",
+        phoneNumber:"081340598743"
+    },
+    {
+        name:"Junior Mewengkang",
+        institution:"Universitas Klabat",
+        address:"Lembean - Minahasa Utara",
+        phoneNumber:"082134982323"
+    },
+    {
+        name:"John Strive",
+        institution:"Universitas Klabat",
+        address:"Lembean - Minahasa Utara",
+        phoneNumber:"081308324043"
+    },
+    {
+        name:"Jack Miller",
+        institution:"Universitas Klabat",
+        address:"Lembean - Minahasa Utara",
+        phoneNumber:"0813xxxxxxxx"
+    }
+];
